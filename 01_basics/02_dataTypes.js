fecd7f22b20a1b43b0abcb6178afeacf29cb7c67
Chirag Patil <140908004+chiragpatil-23@users.noreply.github.com>
2025-02-17 13:56:00 +0000

    typeof null is object

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+ "use strict"; //treat all JS code as newer version
+ 
+//  alert(3+3) //works only in the browser, not in nodejs
+
+console.log(3+3)
+
+let name = "Chirag"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof(age)) // number
+
+console.log(typeof(undefined)) // undefined
+console.log(typeof(null)) // object
