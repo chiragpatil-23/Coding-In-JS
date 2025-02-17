2d5cd7e63064d951540bdfc3e61f96c212c1bac8
Chirag Patil <140908004+chiragpatil-23@users.noreply.github.com>
2025-02-17 03:33:20 +0000

    prefer not to use var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "chirag@gmail.com"
+var accountPassword = "12345" //don't use var
+accountCity = "Pune"
+let accountState
+
+// accountId = 2
+accountEmail = "cp@gmail.com"
+accountPassword = "0978"
+accountCity = "Sangli"
+
+// perfer not to use var
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
